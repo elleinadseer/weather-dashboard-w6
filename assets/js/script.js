6efd8b0463608015e23042e461b7a7ced1d3ac04
elleinadseer <126515415+elleinadseer@users.noreply.github.com>
2023-05-15 00:13:17 +0100

    Search text and search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
-var userCity = function() {
-    var userCityInput = cityInput.value;
+const WEATHER_API_BASE_URL = 'https://api.openweathermap.org'; 
+const WEATHER_API_KEY = 'f23ee9deb4e1a7450f3157c44ed020e1'; 
+const MAX_DAILY_FORECAST = 5;
+const fdList = document.getElementById('five-day'); 
+var forecast = document.getElementById('forecast');
 
-    if (userCityInput === '') {
-        window.alert("Please write a city");
-    }
-}
+// Search Text and Search Button
+
+    var cityInput = document.getElementById('cityInput'); 
+    var searchButton = document.getElementById('searchButton');
+
+    searchButton.addEventListener('click', getCity);
+
+    searchButton.addEventListener("click", function(event){
+        event.preventDefault();
+
+      });
